951f76a44457642292f56dd62e8b19090ddf607b
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2017-04-25 12:57:12 +0200

    add OTP-API-KEY to injectable service

--- src/app/services/oauth-request.service.ts
+++ src/app/services/oauth-request.service.ts
@@ -1,6 +1,7 @@
 import { NgModule, Injectable } from '@angular/core';
 import { RequestOptions, RequestOptionsArgs, RequestMethod, Headers } from '@angular/http';
-import { TokenService } from './auth.service';
+import { TokenService } from 'app/services/auth.service';
+import { environment } from 'environments/environment.dev';
 
 @Injectable()
 export class OauthRequestOptions extends RequestOptions {
@@ -8,11 +9,12 @@ export class OauthRequestOptions extends RequestOptions {
     super();
 
   }
-  merge(options?:RequestOptionsArgs):RequestOptions {
+  merge(options?:RequestOptionsArgs): RequestOptions {
     if (!options.headers) {
       options.headers = new Headers();
     }
     options.headers.append('Authorization', `Bearer ${this.tokenService.token}`);
+    options.headers.append('OTP-API-KEY', environment.OTP_API_KEY)
     return super.merge(options);
   }
 }
