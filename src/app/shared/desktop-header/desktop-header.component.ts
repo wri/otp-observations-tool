25568edb6ffbd44514de52a87ffa808812b3c2d5
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2017-05-03 18:22:41 +0100

    Hide some routes if the user isn't an admin

--- src/app/shared/desktop-header/desktop-header.component.ts
+++ src/app/shared/desktop-header/desktop-header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'app/services/auth.service';
 import { NavigationItem } from 'app/shared/navigation/navigation.component';
 
 @Component({
@@ -6,12 +7,25 @@ import { NavigationItem } from 'app/shared/navigation/navigation.component';
   templateUrl: './desktop-header.component.html',
   styleUrls: ['./desktop-header.component.scss']
 })
-export class DesktopHeaderComponent {
+export class DesktopHeaderComponent implements OnInit {
 
-  private navigationItems: NavigationItem[] = [
-    { name: 'Observations', url: 'observations' },
-    { name: 'Observation fields', url: 'fields' },
-    { name: 'Users', url: 'users' },
-  ];
+  private navigationItems: NavigationItem[] = [];
+
+  constructor (private auth: AuthService) {}
+
+  async ngOnInit() {
+    const isAdmin = await this.auth.isAdmin();
+
+    this.navigationItems = [
+      { name: 'Observations', url: 'observations' },
+      { name: 'Observation fields', url: 'fields' },
+      { name: 'Users', url: 'users' },
+    ];
+
+    if (!isAdmin) {
+      // We just keep the "Observations" tab
+      this.navigationItems = [this.navigationItems[0]];
+    }
+  }
 
 }
