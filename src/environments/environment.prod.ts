a6ccdd064e3ca16be978df7377286b201688d48a
David Inga <david.inga@vizzuality.com>
2017-05-11 18:24:51 +0200

    fixing prod slug

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://52.44.57.176',
+  apiUrl: 'http://52.44.57.176/',
   OTP_API_KEY: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjo1fQ._b6rbfAJBK0oWPUYOUjix2gjFetFrJP9SnNnYZbDn2U'
 };
