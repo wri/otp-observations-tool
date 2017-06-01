db3fc67f38b4b8a5d19469675d3d4cf418df1dec
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2017-06-01 09:38:18 +0100

    Add a language selector in the header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Component, Inject, LOCALE_ID } from '@angular/core';
 import { AuthService } from 'app/services/auth.service';
 import { NavigationItem } from 'app/shared/navigation/navigation.component';
 
@@ -11,13 +12,30 @@ export class HeaderComponent {
 
   private isAdmin = false;
   isLogged = false;
+  lang: string;
 
-  constructor (private authService: AuthService) {
+  constructor (
+    private authService: AuthService,
+    private router: Router,
+    private route: ActivatedRoute,
+    @Inject(LOCALE_ID) locale: string
+  ) {
+    this.lang = locale.slice(0, 2);
+
     // Each time the status of the login change, we update some variables
     this.authService.loginStatus.subscribe(isLogged => {
       this.isLogged = isLogged;
       this.authService.isAdmin().then(isAdmin => this.isAdmin = isAdmin);
     });
+  }
+
+  onChangeLanguage() {
+    // We update the lang query param and reload the app so the other
+    // bundle can be loaded
+    this.router.navigate([], {
+      queryParams: { lang: this.lang },
+      relativeTo: this.route
+    }).then(() => location.reload());
   }
 
   logout(): void {
