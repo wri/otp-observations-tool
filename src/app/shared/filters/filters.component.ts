d06028a3a14fc91728333376d7e6c88a2e1a5890
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2017-06-06 09:58:21 +0100

    Make the filters... filter
    The filters don't get their values yet because the API doesn't provide them. Only the filters with fixed values work.

--- src/app/shared/filters/filters.component.ts
+++ src/app/shared/filters/filters.component.ts
@@ -1,5 +1,5 @@
 import { FilterDirective } from './directives/filter.directive';
-import { Component, ContentChildren, QueryList } from '@angular/core';
+import { Component, ContentChildren, QueryList, Output, EventEmitter } from '@angular/core';
 
 export interface Filter {
   name: string;
@@ -21,26 +21,37 @@ export class FiltersComponent {
   filters: Filter[];
   modalOpen = false;
 
+  @Output() change = new EventEmitter<void>();
+
   @ContentChildren(FilterDirective)
   set filtersNodes(filters: QueryList<FilterDirective>) {
     this._filtersNodes = filters;
-    this.resetFilters();
+    this.resetFilters(true);
   }
 
-  resetFilters() {
+  resetFilters(silent = false) {
     this.filters = this._filtersNodes.toArray().map(filter => ({
       name: filter.name,
       prop: filter.prop,
       values: filter.values || [],
-      selected: filter.default || undefined,
+      selected: filter.default || '',
       required: filter.required || false,
       visible: filter.required || false
     }));
+
+    if (!silent) {
+      this.change.emit();
+    }
   }
 
   onCancel() {
     this.modalOpen = false;
     this.resetFilters();
+  }
+
+  onDone() {
+    this.modalOpen = false;
+    this.change.emit();
   }
 
 }
