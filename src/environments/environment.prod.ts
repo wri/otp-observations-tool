8ec8ce7510fb061c7ffa826183cc661194615e51
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2017-09-04 13:12:38 +0100

    Update the prod credentials to correctly use the staging API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://52.44.57.176/',
-  OTP_API_KEY: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjo1fQ._b6rbfAJBK0oWPUYOUjix2gjFetFrJP9SnNnYZbDn2U'
+  apiUrl: 'http://otp.vizzuality.com/api/',
+  OTP_API_KEY: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjozfQ.1Ze20XwvVchcOzokQAiSsoUYatboK7Ii1E0e8Mqnj38'
 };
