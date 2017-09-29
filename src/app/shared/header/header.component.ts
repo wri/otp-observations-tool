3c1eb3bed6b2fa4086d7bb5c389cbc2a191a111e
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2017-09-29 09:24:30 +0100

    Translate the header component

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,3 +1,4 @@
+import { TranslateService } from '@ngx-translate/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Component } from '@angular/core';
 import { AuthService } from 'app/services/auth.service';
@@ -12,27 +13,51 @@ export class HeaderComponent {
 
   private isAdmin = false;
   isLogged = false;
-  lang = 'en';
+  acceptedLang = ['en', 'fr'];
+  defaultLang = 'en';
+  _lang = this.defaultLang;
+
+  get lang() { return this._lang; }
+  set lang(lang) {
+    if (this._lang === lang) {
+      return;
+    }
+
+    this._lang = lang;
+    this.translateService.use(lang);
+
+    this.router.navigate([], {
+      queryParams: { lang },
+      relativeTo: this.route
+    });
+  }
 
   constructor (
     private authService: AuthService,
     private router: Router,
     private route: ActivatedRoute,
+    private translateService: TranslateService
   ) {
+    // We set the fallback language
+    this.translateService.setDefaultLang('zu');
+
+    // Language to use by default
+    this.translateService.use(this.lang);
+
+    this.route.queryParams.subscribe((params) => {
+      if (params.lang && this.acceptedLang.indexOf(params.lang) !== -1) {
+        this.lang = params.lang;
+      } else {
+        // This will update the URL with a correct lang param
+        this.lang = this.lang;
+      }
+    });
+
     // Each time the status of the login change, we update some variables
     this.authService.loginStatus.subscribe(isLogged => {
       this.isLogged = isLogged;
       this.authService.isAdmin().then(isAdmin => this.isAdmin = isAdmin);
     });
-  }
-
-  onChangeLanguage() {
-    // We update the lang query param and reload the app so the other
-    // bundle can be loaded
-    this.router.navigate([], {
-      queryParams: { lang: this.lang },
-      relativeTo: this.route
-    }).then(() => location.reload());
   }
 
   logout(): void {
