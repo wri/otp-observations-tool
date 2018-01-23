7ebf4b3715a7f7a930cf4c6d34d2dda25843effb
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2018-01-23 17:03:59 +0000

    Show both active and inactive operators in the table

--- src/app/pages/fields/operators/operator-list.component.ts
+++ src/app/pages/fields/operators/operator-list.component.ts
@@ -1,9 +1,10 @@
+import { TranslateService } from '@ngx-translate/core';
 import { AuthService } from 'app/services/auth.service';
 import { TableFilterBehavior } from 'app/shared/table-filter/table-filter.behavior';
 import { OperatorsService } from 'app/services/operators.service';
 import { Operator } from 'app/models/operator.model';
 import { Router, ActivatedRoute } from '@angular/router';
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 export enum OperatorTypes {
   'Artisanal' = 'Artisanal',
@@ -22,18 +23,40 @@ export enum OperatorTypes {
   templateUrl: './operator-list.component.html',
   styleUrls: ['./operator-list.component.scss']
 })
-export class OperatorListComponent extends TableFilterBehavior {
+export class OperatorListComponent extends TableFilterBehavior implements AfterViewInit {
 
   operatorTypes = Object.keys(OperatorTypes);
+  activeFilterValues: any = [];
   isAdmin = this.authService.isAdmin();
 
   constructor(
     protected service: OperatorsService,
     private router: Router,
     private route: ActivatedRoute,
-    public authService: AuthService
+    public authService: AuthService,
+    private translateService: TranslateService
   ) {
     super();
+
+    this.updateActiveFilterValues();
+  }
+
+  ngAfterViewInit(): void {
+    // We set a default filter which is to show the
+    // the operators whether active or not
+    this.filters.getApiParams = () => {
+      const filters = this.filters.filters
+        .filter(filter => filter.selected !== null)
+        .reduce((res, filter) => {
+          return Object.assign({}, res, {
+            [`filter[${filter.prop}]`]: filter.selected
+          });
+        }, {});
+
+      return Object.assign({}, { 'filter[is-active]': 'true, false' }, filters);
+    };
+
+    super.ngAfterViewInit();
   }
 
   triggerNewOperator(): void{
@@ -56,6 +79,28 @@ export class OperatorListComponent extends TableFilterBehavior {
     }
 
     return operator.country.id === this.authService.userCountryId;
+  }
+
+  /**
+   * Update the values for the active filter according to
+   * the current language
+   */
+  async updateActiveFilterValues() {
+    await Promise.all([
+      this.translateService.get('Active').toPromise(),
+      this.translateService.get('Inactive').toPromise()
+    ]).then(([active, inactive]) => {
+      // We sort the values by alphabetical order
+      const values = {
+        [active]: true,
+        [inactive]: false
+      };
+      return Object.keys(values)
+        .sort()
+        .map(key => ({ [key]: values[key] }))
+        .reduce((res, filter) => Object.assign(res, filter), {});
+
+    }).then(activeFilterValues => this.activeFilterValues = activeFilterValues);
   }
 
 }
