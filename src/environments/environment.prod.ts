da88378288c192966ca1572d6212f6e2bb3bc1e4
Clément Prod'homme <contact@clementprodhomme.fr>
2019-07-01 15:16:19 +0100

    Update the URL of the API on production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://otp.vizzuality.com/api/',
+  apiUrl: 'https://otp.vizzuality.com/api/',
   OTP_API_KEY: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjozfQ.q7nr68vfMJqgiu5xyoi9GMD-U0qJLoClxOXL5UswJTg'
 };
