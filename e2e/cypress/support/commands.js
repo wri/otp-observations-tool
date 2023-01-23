2fdf1045a250d77024588c84d63e68fa30b861cb
Tomasz Subik <tsubik@gmail.com>
2023-01-23 21:15:31 +0100

    e2e: add simple observation creation test

--- e2e/cypress/support/commands.js
+++ e2e/cypress/support/commands.js
@@ -8,3 +8,52 @@ Cypress.Commands.add('login', (username, password) => {
   cy.wait(1000);
   cy.contains('Log out');
 });
+
+Cypress.Commands.add('selectOption', (selector, option) => {
+  const options = [].concat(option);
+
+  cy.get(`ss-multiselect-dropdown[ng-reflect-name=${selector}]`)
+    .find("button")
+    .contains('Select')
+    .click();
+
+  options.forEach((option) => {
+    cy.get(`ss-multiselect-dropdown[ng-reflect-name=${selector}]`)
+    .find("li.dropdown-item")
+    .contains(option, { matchCase: false })
+    .click();
+  });
+
+  cy.get(`ss-multiselect-dropdown[ng-reflect-name=${selector}]`).click();
+});
+
+Cypress.Commands.add('expectSelectedOption', (selector, option) => {
+  const options = [].concat(option);
+
+  options.forEach((option) => {
+    cy.get(`ss-multiselect-dropdown[ng-reflect-name=${selector}]`)
+    .find("button.dropdown-toggle")
+    .contains(option)
+    .should('exist');
+  })
+})
+
+Cypress.Commands.add('chooseOption', (question, option) => {
+  cy.get(".form-group")
+    .contains(question, { matchCase: false })
+    .parent('.form-group')
+    .find("label")
+    .contains(option, { matchCase: false })
+    .click();
+});
+
+Cypress.Commands.add('expectChosenOption', (question, option) => {
+  cy.get(".form-group")
+    .contains(question, { matchCase: false })
+    .parent('.form-group')
+    .find("label")
+    .contains(option, { matchCase: false })
+    .parent() // TODO: not sure if ok
+    .find('input[type=radio]')
+    .should('be.checked');
+});
