51e85fad8d9cd6b6b0494cb66fd7a45bd1c1b1a1
Tomasz Subik <tsubik@gmail.com>
2025-05-23 14:21:42 +0200

    add hotjar

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -4,5 +4,7 @@ export const environment = {
   OTP_API_KEY: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjozfQ.1Ze20XwvVchcOzokQAiSsoUYatboK7Ii1E0e8Mqnj38',
   LOCALES_PATH: 'lang',
   GOOGLE_ANALYTICS_ID: 'UA-48182293-6',
-  SENTRY_DSN: 'https://56c3fd67485407b29a717b434e360a67@o1272271.ingest.sentry.io/4505832668463104'
+  SENTRY_DSN: 'https://56c3fd67485407b29a717b434e360a67@o1272271.ingest.sentry.io/4505832668463104',
+  HOTJAR_ID: undefined,
+  HOTJAR_VERSION: undefined
 };
