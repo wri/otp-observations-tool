97703d346d822829b7ad97c7d24cd37d6bd2fc1b
Tomasz Subik <tsubik@gmail.com>
2026-08-18 11:00:28 +0200

    use more direct reset db to save up on every reset - not loading rake task

--- e2e/restore-db.sh
+++ e2e/restore-db.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 set -e
 
-cd ../../otp-api
-RAILS_ENV=e2e bundle exec rails e2e:db_reset
+API="$(dirname "$0")/../../otp-api"
+
+# Fall back to the rake task while the API repo is missing bin/e2e-db-reset.
+if [ -f "$API/bin/e2e-db-reset" ]; then
+  exec sh "$API/bin/e2e-db-reset"
+else
+  cd "$API"
+  exec env RAILS_ENV=e2e bundle exec rails e2e:db_reset
+fi
